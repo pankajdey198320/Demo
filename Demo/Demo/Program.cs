3b77476eb3327518e9494d78a6a10a947038db7c
pdey <pankajd@mindfiresolutions.com>
2015-05-07 10:45:06 +0530

    Some refactor

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Demo
 {
+    /// <summary>
+    /// This is startup point
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
